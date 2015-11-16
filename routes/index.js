e4f2a69199c2ea74484ef973c0d5a9550e3f8284
coolfish <461836324@qq.com>
2015-11-16 14:54:48 +0800

    add error and errorHandler, delete app.js -> 404

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var errorHandler = require('../lib/util/errorHandler');
+
 module.exports = function (app)
 {
     //用来检测运行是否成功
@@ -11,5 +13,7 @@ module.exports = function (app)
     //标签类型请求转发
     app.use('/api/tag', require('../lib/tag/router'));
 
-    //todo 错误拦截
+    // 错误拦截
+    app.use(errorHandler.handler404);
+    app.use(errorHandler.errorHandler);
 };
